75573faec3ce0c3b3d62a94939e9de07f44f483f
J.D. Bean <jonathandbean@gmail.com>
2017-12-20 23:32:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  end
+  elsif int % 3 == 0
     "Fizz"
   end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   end
 end
